c9bfa14ac7cbae963db010c3cccf36fe241932bd
adirifta <adiriftadk@gmail.com>
2024-03-25 16:19:31 +0700

    SplashScreen

--- app/src/main/java/com/example/aplikasi_pensi/MainActivity.kt
+++ app/src/main/java/com/example/aplikasi_pensi/MainActivity.kt
@@ -3,44 +3,77 @@ package com.example.aplikasi_pensi
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.aplikasi_pensi.ui.theme.AplikasiPensiTheme
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AplikasiPensiTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+            Navigation()
+        }
+    }
+}
+
+
+@Composable
+fun Navigation(){
+
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = "splash_screen"
+    ){
+        composable("splash_screen"){
+            SplashScreen(navController = navController)
+        }
+
+        composable("main_screen"){
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                Text(text = "Introduction", fontSize = 30.sp, fontWeight = FontWeight.Bold)
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun SplashScreen(navController: NavController){
+    LaunchedEffect(key1 = true) {
+        delay(5000L)
+        navController.navigate("main_screen")
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AplikasiPensiTheme {
-        Greeting("Android")
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.logo_splashscreen),
+            contentDescription = "",
+            modifier = Modifier.width(350.dp).height(350.dp)
+        )
     }
 }
